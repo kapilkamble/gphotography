936f9ed7317102a4825bdc0462b102798d118dc8
Kapil Kamble <kkamble@collaw.edu.au>
2020-03-29 15:47:53 +1100

    first commit

--- data/config.js
+++ data/config.js
@@ -1,32 +1,32 @@
 module.exports = {
   // Name of your site. Can be the name of a brand, or your personal name.
-  title: 'Gatsby Starter',
+  title: 'Gaurab Photography',
   // What's the purpose of this website? What can we expect from it?
   description:
     `A portfolio based on your latest Instagram posts, implemented with Uber's Base Web Design System.`,
   // Your legal name.
-  legalName: 'Gatsby Starter',
+  legalName: 'Gaurab Photography',
   // URL to this website. If you bought `www.domain.com`, then it will be `www.domain.com`.
-  url: 'https://www.robonomy.com',
+  url: 'http://gaurabphotography.com.au/',
   // Favicon that will display on browsers.
   logo: 'static/images/favicon.png',
   // Who made this website?
-  author: 'Juan Alejandro Morais',
+  author: 'Kapil',
   // What's the website of the author?
-  authorUrl: 'https://www.timrodz.com',
+  authorUrl: 'mailto:askwebdeveloper@outlook.com',
   // Your social media presence, in the form of usernames.
   social: {
     // tells the Instagram API the username to get posts from. 
     // If the url is `https://www.instagram.com/natgeotravel`, your username is `natgeotravel`.
-    instagram: 'natgeotravel', 
+    instagram: 'gaurab_photography', 
     // Optional
-    twitter: '@NatGeoTravel',
+    // twitter: '@NatGeoTravel',
   },
   socialLinks: {
     // Complete URL verison of `social.instagram`.
-    instagram: 'https://www.instagram.com/natgeotravel',
+    instagram: 'https://www.instagram.com/gaurab_photography',
     // Optional
-    twitter: 'https://twitter.com/NatGeoTravel',
+    // twitter: 'https://twitter.com/NatGeoTravel',
   },
   // If applicable, your Google Analytics crawl ID.
   googleAnalyticsId: 'UA-XXXXXXXX-X',
@@ -35,13 +35,13 @@ module.exports = {
   backgroundColor: '#fff',
   // No more information than City + Country due to privacy concerns.
   address: {
-    city: 'City',
-    country: 'Country',
+    city: 'Sydney',
+    country: 'Australia',
   },
   // Basic contact details.
   contact: {
     // Email address to contact you.
-    email: 'timrodz@icloud.com',
+    email: 'gmanandhar@collaw.edu.au',
   },
   // When was this website created?
   foundingDate: '2020',
